817f3c176d70a2677dba34d5e2332f023e2dc728
azkajihan09 <windysabtami92@gmail.com>
2025-11-25 12:36:05 +0800

    ddd

---
+++ application/controllers/Data_Perkara_Gugatan.php
@@ -0,0 +1,185 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Data_Perkara_Gugatan extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model("M_data_perkara_gugatan");
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        // Get filter parameters
+        $wilayah = $this->input->post('wilayah') ?: 'HSU'; // Default HSU
+        $lap_bulan = $this->input->post('lap_bulan') ?: date('m');
+        $lap_tahun = $this->input->post('lap_tahun') ?: date('Y');
+        $jenis_perkara = $this->input->post('jenis_perkara') ?: 'Cerai Gugat';
+        $report_type = $this->input->post('report_type') ?: 'summary';
+
+        $data = array();
+        $data['selected_wilayah'] = $wilayah;
+        $data['selected_bulan'] = $lap_bulan;
+        $data['selected_tahun'] = $lap_tahun;
+        $data['selected_jenis'] = $jenis_perkara;
+        $data['selected_report'] = $report_type;
+
+        // Get data based on report type and region
+        switch ($report_type) {
+            case 'summary':
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_summary_perceraian($lap_bulan, $lap_tahun, $jenis_perkara, $wilayah);
+                break;
+            case 'yearly':
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_yearly_perceraian($lap_tahun, $jenis_perkara, $wilayah);
+                break;
+            case 'monthly':
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_monthly_perceraian($lap_tahun, $jenis_perkara, $wilayah);
+                break;
+            case 'comparison':
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_comparison_gugat_talak($lap_bulan, $lap_tahun, $wilayah);
+                break;
+            case 'faktor':
+                $jenis_kelamin = $this->input->post('jenis_kelamin') ?: '';
+                $data['selected_gender'] = $jenis_kelamin;
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_faktor_perceraian($lap_tahun, $wilayah, $jenis_kelamin);
+                break;
+            case 'faktor_detail':
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_faktor_perceraian_detail($lap_bulan, $lap_tahun, $wilayah);
+                break;
+            default:
+                $data['datafilter'] = $this->M_data_perkara_gugatan->data_summary_perceraian($lap_bulan, $lap_tahun, $jenis_perkara, $wilayah);
+        }
+
+        // Load views
+        $this->load->view('template/new_header');
+        $this->load->view('template/new_sidebar');
+        $this->load->view('v_data_perkara_gugatan', $data);
+        $this->load->view('template/new_footer');
+    }
+
+    // Export functions
+    public function export_excel()
+    {
+        $wilayah = $this->input->post('wilayah') ?: 'HSU';
+        $lap_bulan = $this->input->post('lap_bulan') ?: date('m');
+        $lap_tahun = $this->input->post('lap_tahun') ?: date('Y');
+        $jenis_perkara = $this->input->post('jenis_perkara') ?: 'Cerai Gugat';
+        $report_type = $this->input->post('report_type') ?: 'summary';
+
+        // Get data for export
+        switch ($report_type) {
+            case 'summary':
+                $data = $this->M_data_perkara_gugatan->data_summary_perceraian($lap_bulan, $lap_tahun, $jenis_perkara, $wilayah);
+                break;
+            case 'yearly':
+                $data = $this->M_data_perkara_gugatan->data_yearly_perceraian($lap_tahun, $jenis_perkara, $wilayah);
+                break;
+            case 'monthly':
+                $data = $this->M_data_perkara_gugatan->data_monthly_perceraian($lap_tahun, $jenis_perkara, $wilayah);
+                break;
+            case 'comparison':
+                $data = $this->M_data_perkara_gugatan->data_comparison_gugat_talak($lap_bulan, $lap_tahun, $wilayah);
+                break;
+            case 'faktor':
+                $jenis_kelamin = $this->input->post('jenis_kelamin') ?: '';
+                $data = $this->M_data_perkara_gugatan->data_faktor_perceraian($lap_tahun, $wilayah, $jenis_kelamin);
+                break;
+            case 'faktor_detail':
+                $data = $this->M_data_perkara_gugatan->data_faktor_perceraian_detail($lap_bulan, $lap_tahun, $wilayah);
+                break;
+            default:
+                $data = $this->M_data_perkara_gugatan->data_summary_perceraian($lap_bulan, $lap_tahun, $jenis_perkara, $wilayah);
+        }
+
+        // Load PHPExcel library and create export
+        $this->load->library('PHPExcel');
+        $excel = new PHPExcel();
+
+        // Set document properties
+        $excel->getProperties()
+            ->setCreator("PA Amuntai")
+            ->setTitle("Data Perkara Gugatan - " . ucwords(str_replace('_', ' ', $report_type)))
+            ->setDescription("Laporan Data Perkara Gugatan " . strtoupper($wilayah) . " - " . date('F Y', mktime(0, 0, 0, $lap_bulan, 1, $lap_tahun)));
+
+        $sheet = $excel->getActiveSheet();
+
+        // Set headers based on report type
+        $this->_set_excel_headers($sheet, $report_type);
+
+        // Add data to excel
+        $this->_add_excel_data($sheet, $data, $report_type);
+
+        // Set filename
+        $filename = 'Data_Perkara_Gugatan_' . $wilayah . '_' . $report_type . '_' . $lap_bulan . '_' . $lap_tahun . '.xlsx';
+
+        // Output file
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+
+        $writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+        $writer->save('php://output');
+    }
+
+    private function _set_excel_headers($sheet, $report_type)
+    {
+        switch ($report_type) {
+            case 'summary':
+            case 'yearly':
+            case 'monthly':
+                $sheet->setCellValue('A1', 'Kecamatan');
+                $sheet->setCellValue('B1', 'Perkara Masuk');
+                $sheet->setCellValue('C1', 'Perkara Putus');
+                $sheet->setCellValue('D1', 'Perkara Telah BHT');
+                $sheet->setCellValue('E1', 'Jumlah Akta Cerai');
+                break;
+            case 'comparison':
+                $sheet->setCellValue('A1', 'Kecamatan');
+                $sheet->setCellValue('B1', 'Cerai Gugat');
+                $sheet->setCellValue('C1', 'Cerai Talak');
+                $sheet->setCellValue('D1', 'Total');
+                break;
+            case 'faktor':
+            case 'faktor_detail':
+                $sheet->setCellValue('A1', 'Faktor Perceraian');
+                $sheet->setCellValue('B1', 'Jumlah Kasus');
+                $sheet->setCellValue('C1', 'Persentase');
+                break;
+        }
+    }
+
+    private function _add_excel_data($sheet, $data, $report_type)
+    {
+        $row = 2;
+        foreach ($data as $item) {
+            switch ($report_type) {
+                case 'summary':
+                case 'yearly':
+                case 'monthly':
+                    $sheet->setCellValue('A' . $row, $item->KECAMATAN);
+                    $sheet->setCellValue('B' . $row, $item->PERKARA_MASUK);
+                    $sheet->setCellValue('C' . $row, $item->PERKARA_PUTUS);
+                    $sheet->setCellValue('D' . $row, $item->PERKARA_TELAH_BHT);
+                    $sheet->setCellValue('E' . $row, $item->JUMLAH_AKTA_CERAI);
+                    break;
+                case 'comparison':
+                    $sheet->setCellValue('A' . $row, $item->KECAMATAN);
+                    $sheet->setCellValue('B' . $row, $item->CERAI_GUGAT);
+                    $sheet->setCellValue('C' . $row, $item->CERAI_TALAK);
+                    $sheet->setCellValue('D' . $row, $item->TOTAL);
+                    break;
+                case 'faktor':
+                case 'faktor_detail':
+                    $sheet->setCellValue('A' . $row, isset($item->faktor_perceraian) ? $item->faktor_perceraian : $item->FAKTOR);
+                    $sheet->setCellValue('B' . $row, isset($item->jumlah) ? $item->jumlah : $item->JUMLAH);
+                    $sheet->setCellValue('C' . $row, isset($item->persentase) ? $item->persentase . '%' : (isset($item->PERSENTASE) ? $item->PERSENTASE . '%' : '0%'));
+                    break;
+            }
+            $row++;
+        }
+    }
+}
--- application/views/template/new_sidebar.php
+++ application/views/template/new_sidebar.php
@@ -1,208 +1,239 @@
   <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-  	<!-- Brand Logo -->
-  	<a href="<?php echo site_url('Admin/Dashboard') ?>" class="brand-link">
-  		<img src="<?php echo base_url() ?>assets/dist/img/logo-mahkamah-agung.png" alt="Logo PA Amuntai" class="brand-image img-circle elevation-3" style="opacity: .8">
-  		<span class="brand-text font-weight-light">LAPORAN PERKARA</span>
-  	</a>
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+	<!-- Brand Logo -->
+	<a href="<?php echo site_url('Admin/Dashboard') ?>" class="brand-link">
+		<img src="<?php echo base_url() ?>assets/dist/img/logo-mahkamah-agung.png" alt="Logo PA Amuntai" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<span class="brand-text font-weight-light">LAPORAN PERKARA</span>
+	</a>
 
-  	<!-- Sidebar -->
-  	<div class="sidebar">
-  		<!-- Sidebar user (optional) -->
-  		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-  			<div class="image">
-  				<img src="<?php echo base_url() ?>assets/dist/img/Logo PA Amuntai - Trans.png" class="img-circle elevation-2" alt="User Image">
-  			</div>
-  			<div class="info">
-  				<a href="#" class="d-block">PA Amuntai</a>
-  			</div>
-  		</div>
+	<!-- Sidebar -->
+	<div class="sidebar">
+		<!-- Sidebar user (optional) -->
+		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+			<div class="image">
+				<img src="<?php echo base_url() ?>assets/dist/img/Logo PA Amuntai - Trans.png" class="img-circle elevation-2" alt="User Image">
+			</div>
+			<div class="info">
+				<a href="#" class="d-block">PA Amuntai</a>
+			</div>
+		</div>
 
+		<!-- Sidebar Menu -->
+		<nav class="mt-2">
+			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-
-
-  		<!-- Sidebar Menu -->
-  		<nav class="mt-2">
-  			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-
-  				<li class="nav-item">
-  					<a href="#" class="nav-link">
-  						<i class="nav-icon fas fa-chart-area"></i>
-  						<p>
-  							Dashboard
-  							<i class="right fas fa-angle-left"></i>
-  						</p>
-  					</a>
-  					<ul class="nav nav-treeview">
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Dashboard/simple') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>Dashboard Simple</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Dashboard') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>Dashboard Modern</p>
-  							</a>
-  						</li>
-  					</ul>
-  				</li>
-
-  				<li class="nav-item">
-  					<a href="#" class="nav-link">
-  						<i class="nav-icon fas fa-folder"></i>
-  						<p>
-  							DELEGASI
-  							<i class="fas fa-angle-left right"></i>
-  						</p>
-  					</a>
-  					<ul class="nav nav-treeview">
-  						<li class="nav-item active">
-  							<a href="<?php echo site_url('Delegasi') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>Delegasi Masuk</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Delegasi_k') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>Delegasi Keluar</p>
-  							</a>
-  						</li>
-  					</ul>
-  				</li>
-  				<li class="nav-item">
-  					<a href="#" class="nav-link">
-  						<i class="nav-icon fas fa-folder"></i>
-  						<p>
-  							DATA PERCERAIAN
-  							<i class="fas fa-angle-left right"></i>
-  						</p>
-  					</a>
-  					<ul class="nav nav-treeview">
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Data_Perceraian_hsu') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>HULU SUNGAI UTARA
-  								</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Data_Perceraian_balangan') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>BALANGAN
-  								</p>
-  							</a>
-  						</li>
-
-  					</ul>
-  				</li>
-
-  				<li class="nav-item">
-  					<a href="#" class="nav-link">
-  						<i class="nav-icon fas fa-folder"></i>
-  						<p>
-  							AKTA CERAI
-  							<i class="fas fa-angle-left right"></i>
-  						</p>
-  					</a>
-  					<ul class="nav nav-treeview">
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Penerbitan_akta_cerai') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>LAPORAN PENERBITAN AKTA CERAI
-  								</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Penyerahan_akta_cerai') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>LAPORAN PENYERAHAN AKTA CERAI
-  								</p>
-  							</a>
-  						</li>
-
-  					</ul>
-  				</li>
-  				<li class="nav-item">
-  					<a href="#" class="nav-link">
-  						<i class="nav-icon fas fa-folder"></i>
-  						<p>
-  							PERKARA
-  							<i class="fas fa-angle-left right"></i>
-  						</p>
-  					</a>
-  					<ul class="nav nav-treeview">
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Lipa1') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>LAPORAN KEADAAN PERKARA</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Perkara_Banding') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>PERKARA BANDING</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Perkara_Putusan') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>PERKARA PUTUSAN</p>
-  							</a>
-  						</li>
+				<!-- Dashboard Menu -->
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-tachometer-alt"></i>
+						<p>
+							Dashboard
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
 						<li class="nav-item">
-  					<a href="#" class="nav-link">
-  						<i class="nav-icon fas fa-folder"></i>
-  						<p>
-  							DELEGASI
-  							<i class="fas fa-angle-left right"></i>
-  						</p>
-  					</a>
-  					<ul class="nav nav-treeview">
-  						<li class="nav-item active">
-  							<a href="<?php echo site_url('Delegasi') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>Delegasi Masuk</p>
-  							</a>
-  						</li>
-  						<li class="nav-item">
-  							<a href="<?php echo site_url('Delegasi_k') ?>" class="nav-link">
-  								<i class="far fa-circle nav-icon"></i>
-  								<p>Delegasi Keluar</p>
-  							</a>
-  						</li>
-  					</ul>
-  				</li>
-  					</ul>
+							<a href="<?php echo site_url('Dashboard/simple') ?>" class="nav-link">
+								<i class="fas fa-chart-line nav-icon"></i>
+								<p>Dashboard Simple</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo site_url('Dashboard') ?>" class="nav-link">
+								<i class="fas fa-chart-area nav-icon"></i>
+								<p>Dashboard Modern</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
+				<!-- Delegasi Menu -->
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-exchange-alt"></i>
+						<p>
+							DELEGASI
+							<i class="fas fa-angle-left right"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="<?php echo site_url('Delegasi') ?>" class="nav-link">
+								<i class="fas fa-arrow-down nav-icon"></i>
+								<p>Delegasi Masuk</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo site_url('Delegasi_k') ?>" class="nav-link">
+								<i class="fas fa-arrow-up nav-icon"></i>
+								<p>Delegasi Keluar</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
-  			</ul>
-  			</li>
-  			</ul>
-  		</nav>
-  		<!-- /.sidebar-menu -->
-  	</div>
-  	<!-- /.sidebar -->
-  </aside>
+				<!-- Data Perceraian Menu -->
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-heart-broken"></i>
+						<p>
+							DATA PERCERAIAN
+							<i class="fas fa-angle-left right"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="<?php echo site_url('Data_Perceraian_hsu') ?>" class="nav-link">
+								<i class="fas fa-map-marker-alt nav-icon"></i>
+								<p>HULU SUNGAI UTARA</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo site_url('Data_Perceraian_balangan') ?>" class="nav-link">
+								<i class="fas fa-map-marker-alt nav-icon"></i>
+								<p>BALANGAN</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+				<!-- Akta Cerai Menu -->
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-certificate"></i>
+						<p>
+							AKTA CERAI
+							<i class="fas fa-angle-left right"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="<?php echo site_url('Penerbitan_akta_cerai') ?>" class="nav-link">
+								<i class="fas fa-file-contract nav-icon"></i>
+								<p>LAPORAN PENERBITAN AKTA CERAI</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo site_url('Penyerahan_akta_cerai') ?>" class="nav-link">
+								<i class="fas fa-handshake nav-icon"></i>
+								<p>LAPORAN PENYERAHAN AKTA CERAI</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
-  <script>
-  	$(document).ready(function() {
-  		$('.nav-link').on('click', function() {
-  			$('.nav-link').removeClass('active');
-  			$(this).addClass('active');
+				<!-- Perkara Menu -->
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-gavel"></i>
+						<p>
+							PERKARA
+							<i class="fas fa-angle-left right"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="<?php echo site_url('Lipa1') ?>" class="nav-link">
+								<i class="fas fa-clipboard-list nav-icon"></i>
+								<p>LAPORAN KEADAAN PERKARA</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo site_url('Perkara_Banding') ?>" class="nav-link">
+								<i class="fas fa-balance-scale nav-icon"></i>
+								<p>PERKARA BANDING</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?php echo site_url('Perkara_Putusan') ?>" class="nav-link">
+								<i class="fas fa-file-signature nav-icon"></i>
+								<p>PERKARA PUTUSAN</p>
+							</a>
+						</li>
+					</ul>
+				</li>
 
+			</ul>
+		</nav>
+		<!-- /.sidebar-menu -->
+	</div>
+	<!-- /.sidebar -->
+</aside>
 
-  		});
-  	});
+<!-- AdminLTE JS diperlukan untuk treeview -->
+<script src="<?php echo base_url() ?>assets/dist/js/adminlte.min.js"></script>
 
-  	$(document).ready(function() {
-  		$('.nav-link').on('click', function() {
-  			$('.nav-item').removeClass('active'); // Menghapus kelas 'active' dari semua item
-  			$(this).parent().addClass('active'); // Menambahkan kelas 'active' ke item yang diklik
-  		});
-  	});
-  </script>
+<script>
+$(document).ready(function() {
+    // Untuk parent menu yang memiliki submenu
+    $('a[href="#"]').on('click', function(e) {
+        e.preventDefault();
+        
+        var $parentLi = $(this).parent('li');
+        var $submenu = $(this).next('ul.nav-treeview');
+        
+        if ($submenu.length > 0) {
+            // Toggle submenu
+            if ($parentLi.hasClass('menu-open')) {
+                $parentLi.removeClass('menu-open');
+                $submenu.slideUp(300);
+            } else {
+                // Tutup submenu lain yang terbuka
+                $('.nav-item.menu-open').removeClass('menu-open');
+                $('.nav-treeview').slideUp(300);
+                
+                // Buka submenu ini
+                $parentLi.addClass('menu-open');
+                $submenu.slideDown(300);
+            }
+        }
+    });
+    
+    // Set active menu berdasarkan URL saat ini
+    var currentPath = window.location.pathname;
+    var currentController = currentPath.split('/').pop(); // Ambil controller dari URL
+    
+    // Reset semua active state
+    $('.nav-link').removeClass('active');
+    $('.nav-item').removeClass('menu-open active');
+    
+    // Cek setiap link submenu
+    $('.nav-treeview .nav-link').each(function() {
+        var href = $(this).attr('href');
+        if (href && href !== '#') {
+            // Ambil bagian controller dari href
+            var linkController = href.split('/').pop();
+            
+            // Jika controller cocok dengan URL saat ini
+            if (currentPath.indexOf(linkController) > -1 || currentController === linkController) {
+                // Set active untuk submenu
+                $(this).addClass('active');
+                
+                // Set active dan buka parent menu
+                var $parentItem = $(this).closest('li.nav-item').parent().closest('li.nav-item');
+                $parentItem.addClass('menu-open active');
+                $parentItem.find('> a').addClass('active');
+                
+                // Tampilkan submenu
+                $(this).closest('.nav-treeview').show();
+            }
+        }
+    });
+});
+</script>
+
+<!-- CSS tambahan untuk smooth animation -->
+<style>
+.nav-treeview {
+    display: none;
+}
+.nav-item.menu-open > .nav-treeview {
+    display: block;
+}
+.nav-sidebar .nav-link {
+    transition: all 0.3s ease;
+}
+.nav-sidebar .nav-link:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+}
+</style>
